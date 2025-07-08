7adbb9effc23d0756c2c8bcc08278546afe90ee3
Alan Philip <alanphilipdev@gmail.com>
2025-07-08 13:42:33 +0200

    update wokrder

--- worker.js
+++ worker.js
@@ -2,17 +2,15 @@ export default {
     async fetch(request) {
         const asn = request.cf?.asn || 'unknown';
 
-        // Fetch the original request
         const response = await fetch(request);
-
-        // Clone response and inject header
         const newHeaders = new Headers(response.headers);
         newHeaders.set('X-ASN', asn);
+        newHeaders.set('X-Worker-Active', 'yes');
 
         return new Response(response.body, {
             status: response.status,
             statusText: response.statusText,
-            headers: newHeaders
+            headers: newHeaders,
         });
     }
 }

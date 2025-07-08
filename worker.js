9036d824516362590362e71150216fcb171e48c1
Alan Philip <alanphilipdev@gmail.com>
2025-07-08 14:23:03 +0200

    allow x-asn header

--- worker.js
+++ worker.js
@@ -4,8 +4,8 @@ export default {
 
         const response = await fetch(request);
         const newHeaders = new Headers(response.headers);
-        newHeaders.set('X-ASN', asn);
-        newHeaders.set('X-Worker-Active', 'yes');
+        newHeaders.set('x-asn', asn);
+        newHeaders.set('access-control-expose-headers', 'x-asn');
 
         return new Response(response.body, {
             status: response.status,
